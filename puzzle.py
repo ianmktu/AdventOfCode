c4a3ab25dba4302cd6206444cc5caba22d519823
Ian Tu <i.tu@warwick.ac.uk>
2020-12-08 02:23:57 +0000

    Add files via upload

--- puzzle.py
+++ puzzle.py
@@ -1,4 +1,6 @@
 import numpy as np
+import anytree
+from anytree import Node, RenderTree
 import data
 
 
@@ -324,7 +326,95 @@ def day6():
 
 
 def day7():
-    current = data.day7()
+    bags = data.day7()
+
+    bag_dict = {}
+    for index, bag_description in enumerate(bags):
+        bag_description_space_split = bag_description.split(" ")
+        current_bag_name = bag_description_space_split[0] + " " + bag_description_space_split[1]
+        bag_contains_list = bag_description.replace(current_bag_name + " bags contain ", "").replace("no other bags", "0 zero things").replace(" bags", "").replace(" bag", "").replace(".", "").split(", ")
+        
+        if current_bag_name not in bag_dict:
+            bag_dict[current_bag_name] = {}
+            bag_dict[current_bag_name]['children'] = []
+            bag_dict[current_bag_name]['parent'] = []
+            bag_dict[current_bag_name]['child_count'] = []
+
+
+        for inner_bag_description in bag_contains_list:
+            inner_bag_description_space_split = inner_bag_description.split(" ")
+            current_bag_count = int(inner_bag_description_space_split[0])
+            current_inner_bag_name = inner_bag_description_space_split[1] + " " + inner_bag_description_space_split[2]
+            
+            if current_bag_count > 0:
+                bag_dict[current_bag_name]['children'].append(current_inner_bag_name)
+                bag_dict[current_bag_name]['child_count'].append(current_bag_count)
+
+            if current_inner_bag_name not in bag_dict:
+                bag_dict[current_inner_bag_name] = {}
+                bag_dict[current_inner_bag_name]['children'] = []
+                bag_dict[current_inner_bag_name]['parent'] = []
+                bag_dict[current_inner_bag_name]['child_count'] = []
+
+            bag_dict[current_inner_bag_name]['parent'].append(current_bag_name)
+
+
+    node_dict = {}
+    root_node = Node('root')
+    for bag_name, adjacency_set_dict in bag_dict.items():
+        parent_node_names = list(adjacency_set_dict['parent'])
+
+        if len(parent_node_names) == 0:
+            parent_node = Node(bag_name, parent=root_node)
+            children_node_names = [(child_bag_name, adjacency_set_dict['child_count'][index], parent_node) for index, child_bag_name in enumerate(adjacency_set_dict['children'])]
+
+            while len(children_node_names) > 0:
+                current_child_bag_name, current_child_bag_count, current_parent_bag_node = children_node_names.pop(0)
+                current_child_node = Node(current_child_bag_name, parent=current_parent_bag_node, count=current_child_bag_count)
+                current_child_node_names = [(child_bag_name, bag_dict[current_child_bag_name]['child_count'][index], current_child_node) for index, child_bag_name in enumerate(bag_dict[current_child_bag_name]['children'])]
+                children_node_names += current_child_node_names
+        else:
+            continue
+    
+    shiny_nodes = anytree.search.findall_by_attr(root_node, 'shiny gold', name='name')
+    shiny_names = set()
+    for shiny_node in shiny_nodes:
+        for shiny_ancestor_node in shiny_node.ancestors:
+            shiny_names.add(shiny_ancestor_node.name)
+    shiny_names.remove("root")
+    
+    def get_node_count(current_node, count):   
+        if len(current_node.children) == 0:    
+            return current_node.count
+        else:
+            current_total = 0
+            for current_child_node in current_node.children:
+                current_total += get_node_count(current_child_node, count) 
+            current_total *= current_node.count
+            current_total += current_node.count
+            return current_total 
+
+    shiny_count = 0 
+    for child_node in shiny_nodes[0].children:
+        shiny_count += get_node_count(child_node, 0)
+
+    print("\n****************************************************")
+    print("\nDay 7: Part 1")
+    print("Contain Shiny Gold Bag Count: {}".format(len(shiny_names)))
+
+    print("\nDay 7: Part 2")
+    print("In Shiny Gold Bag Count: {}".format(shiny_count))
+
+
+def day8():
+    puzzle_input = data.day8()
+
+    print("\n****************************************************")
+    print("\nDay 8: Part 1")
+    print("Answer: {}".format(0000))
+
+    print("\nDay 8: Part 2")
+    print("Answer: {}".format(0000))
 
 
 if __name__ == '__main__':
@@ -335,3 +425,21 @@ if __name__ == '__main__':
     # day5()
     # day6()
     day7()
+    # day8()
+    # day9()
+    # day10()
+    # day11()
+    # day12()
+    # day13()
+    # day14()
+    # day15()
+    # day16()
+    # day17()
+    # day18()
+    # day19()
+    # day20()
+    # day21()
+    # day22()
+    # day23()
+    # day24()
+    # day25()
