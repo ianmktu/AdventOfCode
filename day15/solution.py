0dc21215a8eb5eeed4779f54fb72264fb585d475
Ian Tu <ian_tu@hotmail.co.uk>
2020-12-14 18:07:24 +0000

    Placeholder for Day 15

---
+++ day15/solution.py
@@ -0,0 +1,25 @@
+import copy
+
+import numpy as np
+
+import anytree
+from anytree import Node, RenderTree
+
+
+def day15():
+    lines = [line.rstrip() for line in open('input.txt')]
+
+    for line in lines:
+        continue
+
+    print("\n****************************************************")
+    print("\nDay 15: Part 1")
+    print("Answer: {}".format(0000))
+
+    print("\nDay 15: Part 2")
+    print("Answer: {}".format(0000))
+    print("\n****************************************************")
+
+
+if __name__ == '__main__':
+    day15()
