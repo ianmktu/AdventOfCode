00b07109d4fb0f94760ec95ddae3da9140cefcf9
Ian Tu <ian_tu@hotmail.co.uk>
2020-12-16 02:39:24 +0000

    Made faster

--- day15/solution.py
+++ day15/solution.py
@@ -1,50 +1,31 @@
 def day15():
     game = [0,1,4,13,15,12,16]
+          
+    result = []
+    for i in [2020, 30000000]:
+        game_dict = {}
+        for index, num in enumerate(game):
+            game_dict[num] = index
+        start_index = len(game)  
+        previous_value = game[-1]
+
+        for index in range(start_index, i):      
+            if previous_value in game_dict:
+                new_value = index - 1 - game_dict[previous_value]   
+                game_dict[previous_value] = index - 1
+                previous_value = new_value
+            else:
+                game_dict[previous_value] = index - 1
+                previous_value = 0
+                        
+        result.append(previous_value)
     
-    game_dict = {}
-    for index, num in enumerate(game):
-        game_dict[num] = [index, -1]
-
-    index = len(game)
-    previous_value = game[-1]
-    while True:        
-        if index % 10000 == 0:
-            print()
-            print(index)
-        if previous_value in game_dict and game_dict[previous_value][1] != -1:
-            new_value = game_dict[previous_value][1] - game_dict[previous_value][0]
-            if new_value not in game_dict:
-                game_dict[new_value] = [index, -1]
-            else:
-                if game_dict[new_value][1] == -1:
-                    game_dict[new_value][1] = index
-                else:
-                    game_dict[new_value][0] = game_dict[new_value][1]
-                    game_dict[new_value][1] = index 
-            previous_value = new_value
-        else:
-            if game_dict[0][1] == -1:
-                game_dict[0][1] = index
-            else:
-                game_dict[0][0] = game_dict[0][1]
-                game_dict[0][1] = index
-            previous_value = 0
-                     
-        if index == 2020 - 1:
-            part1 = previous_value
-        elif index == 30000000 - 1:
-            part2 = previous_value
-            break
-        
-        index += 1
-
-
     print("\n****************************************************")
     print("\nDay 15: Part 1")
-    print("Answer: {}".format(part1))
+    print("Answer: {}".format(result[0]))
 
     print("\nDay 15: Part 2")
-    print("Answer: {}".format(part2))
+    print("Answer: {}".format(result[1]))
     print("\n****************************************************")
 
 
