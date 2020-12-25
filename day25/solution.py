4c812e873d2e545e55cca48c55f1aae6c647c94b
Ian Tu <ian_tu@hotmail.co.uk>
2020-12-25 05:41:17 +0000

    Merry Christmas. Completed Day 25!

--- day25/solution.py
+++ day25/solution.py
@@ -1,13 +1,32 @@
 def day25():
-    data = [line.rstrip() for line in open('input.txt')]
+    public_keys = [line.rstrip() for line in open('input.txt')]
+    card_public_key = int(public_keys[0])
+    door_public_key = int(public_keys[1])
+    
+    loop_size = 20000000000
+    subject_number = 7
+    value = 1
+    card_loop_size = 0
+    door_loop_size = 0
+    for i in range(loop_size):
+        value *= subject_number
+        value %= 20201227
+        if value == card_public_key:
+            card_loop_size = i+1
+        if value == door_public_key:
+            door_loop_size = i+1
+        if card_loop_size > 0 and door_loop_size > 0:
+            break
+        
+    subject_number = door_public_key
+    encryption_key = 1
+    for i in range(card_loop_size):
+        encryption_key *= subject_number
+        encryption_key %= 20201227
               
     print("\n****************************************************")
-    print("\nDay 25: Part 1")
-    print("Answer: {}".format(0000))
-
-    print("\nDay 25: Part 2")
-    print("Answer: {}".format(0000))
-    print("\n****************************************************")
+    print("\nDay 25")
+    print("Answer: {}".format(encryption_key))
 
 
 if __name__ == '__main__':
--- puzzle.py
+++ puzzle.py
@@ -2349,7 +2349,35 @@ def day24():
 
 
 def day25():
-    current_data = data.day25()
+    public_keys = data.day25()
+    card_public_key = int(public_keys[0])
+    door_public_key = int(public_keys[1])
+    
+    loop_size = 20000000000
+    subject_number = 7
+    value = 1
+    card_loop_size = 0
+    door_loop_size = 0
+    for i in range(loop_size):
+        value *= subject_number
+        value %= 20201227
+        if value == card_public_key:
+            card_loop_size = i+1
+        if value == door_public_key:
+            door_loop_size = i+1
+        if card_loop_size > 0 and door_loop_size > 0:
+            break
+        
+    subject_number = door_public_key
+    encryption_key = 1
+    for i in range(card_loop_size):
+        encryption_key *= subject_number
+        encryption_key %= 20201227
+              
+    print("\n****************************************************")
+    print("\nDay 25")
+    print("Answer: {}".format(encryption_key))
+    print("\n****************************************************")
 
 
 if __name__ == '__main__':
@@ -2377,5 +2405,5 @@ if __name__ == '__main__':
     # day21()
     # day22()
     # day23()
-    day24()
-    # day25()
+    # day24()
+    day25()
